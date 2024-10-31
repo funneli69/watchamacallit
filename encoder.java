aff3cef4b2e6e5e7d9be72d0feb38058b8d1652a
funneli69 <elim@quadprep.org>
2024-10-31 09:28:54 -0400

    Add files via upload

---
+++ encoder.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class encoder {
+	public static void main(String[]args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your message: ");
+		String message = scanner.nextLine();
+		System.out.println("Enter the key:  ");
+		int key = scanner.nextInt();
+		String encode="";
+		for(int i =0;i <message.length(); i++) {
+			char letter = message.charAt(i);
+			char encodedLetter= (char)(letter+key);
+			encode += encodedLetter;
+		}
+		System.out.println(encode);
+		scanner.close();
+	}
+
+}
+
